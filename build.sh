8adcbdd1a685dafd1aff8d7a76db47f317ac2f39
Taewase <maiwasez47@gmail.com>
2025-07-14 20:20:51 +0200

    Update requirements.txt and build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,11 @@
 export SKIP_RUST=1
 export CRYPTOGRAPHY_DONT_BUILD_RUST=1
 
+# Install an older version of pip that's compatible with Python 3.13
+python -m pip install pip==23.0.1
+
+# Install setuptools and wheel first
+pip install --upgrade pip setuptools wheel
+
 # Install dependencies with --no-build-isolation to avoid Rust
 pip install --no-build-isolation -r requirements.txt
