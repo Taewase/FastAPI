d3649a653f542381bfbf1a84c8ed3e7bba2026bb
Taewase <maiwasez47@gmail.com>
2025-07-14 19:37:42 +0200

    added a build file

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+
+# Install dependencies
+pip install -r requirements.txt
